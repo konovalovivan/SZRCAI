380629096a0de6356e6d6b802c7912756a7c25da
AsahiOcean <asahiocean@icloud.com>
2021-03-10 06:14:39 +0500

    2

--- SZRCAI/SZRCAI/ViewController.swift
+++ SZRCAI/SZRCAI/ViewController.swift
@@ -1,19 +1,8 @@
-//
-//  ViewController.swift
-//  SZRCAI
-//
-//  Created by Sergey Fedotov on 10.03.2021.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
-
