6a63d03f24c122492e550f9202b62ee3edea931a
AsahiOcean <asahiocean@icloud.com>
2021-03-10 06:33:40 +0500

    MKMarkerAnnotationView

---
+++ SZRCAI/SZRCAI/Main code/Views/PinMarkerView/PinMarkerView.swift
@@ -0,0 +1,13 @@
+import Foundation
+import MapKit
+
+class PinMarkerView: MKMarkerAnnotationView {
+    
+    override init(annotation: MKAnnotation?, reuseIdentifier: String?) {
+        super.init(annotation: annotation, reuseIdentifier: reuseIdentifier)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
