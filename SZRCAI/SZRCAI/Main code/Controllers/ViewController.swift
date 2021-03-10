8949a7578fcce0476218cdde5e036cf9d0671df1
AsahiOcean <asahiocean@icloud.com>
2021-03-10 06:45:34 +0500

    Added a map

--- SZRCAI/SZRCAI/Main code/Controllers/ViewController.swift
+++ SZRCAI/SZRCAI/Main code/Controllers/ViewController.swift
@@ -1,10 +1,23 @@
 import UIKit
+import MapKit
 
 // Тестовое задание «СЗ РЦАИ» iOS Developer
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate {
+    
+    var mapView: MapView!
+    
+    /// Button for building graphs and routes
+    weak var mainButton: UIButton!
+    weak var clearButton: UIButton!
+    
+    var buttonKVO: NSKeyValueObservation!
+    var pinsKVO: NSKeyValueObservation!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        mapView = .init(frame: view.frame)
+        mapView.delegate = self
+        view.addSubview(mapView)
     }
 }
